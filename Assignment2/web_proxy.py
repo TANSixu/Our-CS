250804d8a24cc37b948f7b7671fb0e70547d840f
TANSixu <sixu.tan@foxmail.com>
2023-08-01 23:27:42 +0800

    for learner

--- Assignment2/web_proxy.py
+++ Assignment2/web_proxy.py
@@ -4,15 +4,6 @@ import urllib.parse
 
 host = ('localhost', 4242)
 
-def request_web(url):
-    req = urllib.request.Request(url)
-    # req.set_proxy(proxy, "https")
-
-    resp = urllib.request.urlopen(req)
-    html = resp.read().decode("utf-8")
-    print(html)
-
-    return html
  
 class MyResquest(BaseHTTPRequestHandler):
     timeout = 5
@@ -30,17 +21,7 @@ class MyResquest(BaseHTTPRequestHandler):
 
     def do_POST(self):
         post_data = self.rfile.read(int(self.headers['content-length'])).decode()    # Read from post
-        print(post_data)
- 
-        self.send_response(200)
-        self.send_header("Content-type","text/html")    # Set response header
-        self.end_headers()
- 
-        url_raw = post_data.split("&")[0].split("=")[1]
-        url = urllib.parse.unquote(url_raw)
-        html = request_web(url)
         
-        self.wfile.write(html.encode())
  
 if __name__ == '__main__':
     server = HTTPServer(host, MyResquest)
